fbf96827ce7c797dc545af8af1b9d94cff549dd7
tejasv007 <handge.tejaswee@gmail.com>
2025-11-09 20:30:33 +0530

    connectivity

---
+++ connectivity/first.py
@@ -0,0 +1,6 @@
+# # firstly for connectivity, import library
+# import mysql.connector
+# mydb=mysql.connector.connect(host="localhost",user="root",password="Handge@2698#")
+# mycursor=mydb.cursor()
+# mycursor.execute('create database Bittu')
+# print("MySQL connector imported successfully!")
